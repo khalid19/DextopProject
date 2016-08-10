14a73f29b8f8b77f9266e661fec306966903ff99
Your Name <you@example.com>
2016-08-10 09:27:16 +0600

    Dexktop
    Destop test project

---
+++ DesktopApplicationTest/DesktopApplicationTest/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace DesktopApplicationTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.seachButton = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.showListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.closeButton);
+            this.groupBox1.Controls.Add(this.idTextBox);
+            this.groupBox1.Controls.Add(this.seachButton);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.pathTextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.deleteButton);
+            this.groupBox1.Controls.Add(this.updateButton);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.browseButton);
+            this.groupBox1.Controls.Add(this.pictureBox);
+            this.groupBox1.Controls.Add(this.addressTextBox);
+            this.groupBox1.Controls.Add(this.contactTextBox);
+            this.groupBox1.Controls.Add(this.lastNameTextBox);
+            this.groupBox1.Controls.Add(this.firstNameTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(840, 290);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Person Information";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(751, 37);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 12;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(507, 234);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 21);
+            this.idTextBox.TabIndex = 11;
+            // 
+            // seachButton
+            // 
+            this.seachButton.Location = new System.Drawing.Point(618, 234);
+            this.seachButton.Name = "seachButton";
+            this.seachButton.Size = new System.Drawing.Size(75, 23);
+            this.seachButton.TabIndex = 10;
+            this.seachButton.Text = "Search";
+            this.seachButton.UseVisualStyleBackColor = true;
+            this.seachButton.Click += new System.EventHandler(this.seachButton_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(420, 238);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(81, 15);
+            this.label13.TabIndex = 9;
+            this.label13.Text = "Search By Id :";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(423, 176);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(135, 21);
+            this.pathTextBox.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(332, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Path Of Image";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(313, 238);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(220, 238);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(25, 238);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(618, 140);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(377, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(216, 122);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(106, 173);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(145, 21);
+            this.addressTextBox.TabIndex = 3;
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(106, 130);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(145, 21);
+            this.contactTextBox.TabIndex = 2;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(106, 85);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(145, 21);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(106, 41);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(145, 21);
+            this.firstNameTextBox.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(332, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Photo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Contact";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(123, 238);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 13;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // showListView
+            // 
+            this.showListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.showListView.GridLines = true;
+            this.showListView.Location = new System.Drawing.Point(9, 342);
+            this.showListView.Name = "showListView";
+            this.showListView.Size = new System.Drawing.Size(840, 97);
+            this.showListView.TabIndex = 1;
+            this.showListView.UseCompatibleStateImageBehavior = false;
+            this.showListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 48;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 149;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            this.columnHeader3.Width = 170;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Contact";
+            this.columnHeader4.Width = 142;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Address";
+            this.columnHeader5.Width = 79;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Image Location";
+            this.columnHeader6.Width = 320;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 470);
+            this.Controls.Add(this.showListView);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Test Application";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Button seachButton;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListView showListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ DesktopApplicationTest/DesktopApplicationTest/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DesktopApplicationTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = " Image Files(*.jpg;*.jpeg;*.gif;*.bmp)|*.jpg;*.jpeg;*.gif;*.bmp";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+
+                //pictureBox.ImageLocation = open.FileName;
+                pictureBox.ImageLocation = open.FileName;
+                pathTextBox.Text = open.FileName;
+
+                pictureBox.ImageLocation = pathTextBox.Text;
+
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = ConnectionString();
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            Person person = new Person();
+
+            person.FirstName = firstNameTextBox.Text;
+            person.LastName = lastNameTextBox.Text;
+            person.Contact = contactTextBox.Text;
+            person.Address = addressTextBox.Text;
+
+            try
+            {
+                connection.Open();
+                string query = "insert into Person values('" + person.FirstName + "','" + person.LastName + "','" +
+                               person.Contact + "','" + person.Address + "','" + pathTextBox.Text + "')";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                int affectedRow = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                firstNameTextBox.Text = "";
+                lastNameTextBox.Text = "";
+                contactTextBox.Text = "";
+                addressTextBox.Text = "";
+                pictureBox.Refresh();
+
+                MessageBox.Show("Data Saved Successfully" + "\n" + affectedRow + " Row Affected");
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private static string ConnectionString()
+        {
+            string connectionString = "server=DESKTOP-30ONMO9;initial catalog=Test1; integrated security=sspi";
+            return connectionString;
+        }
+
+
+
+        private void seachButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = ConnectionString();
+
+            Person person = new Person();
+
+            person.PersonId = Convert.ToInt32(idTextBox.Text);
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string query = "select * from Person where PersonId='" + person.PersonId + "'";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    person.FirstName = reader[1].ToString();
+                    person.LastName = reader[2].ToString();
+                    person.Contact = reader[3].ToString();
+                    person.Address = reader[4].ToString();
+                    pathTextBox.Text = (reader[5]).ToString();
+
+
+
+                }
+
+                firstNameTextBox.Text = person.FirstName;
+                lastNameTextBox.Text = person.LastName;
+                contactTextBox.Text = person.Contact;
+                addressTextBox.Text = person.Address;
+                pictureBox.ImageLocation = pathTextBox.Text;
+
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message + "Not working properly");
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = ConnectionString();
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            Person person = new Person();
+            person.PersonId = Convert.ToInt32(idTextBox.Text);
+            person.FirstName = firstNameTextBox.Text;
+            person.LastName = lastNameTextBox.Text;
+            person.Contact = contactTextBox.Text;
+            person.Address = addressTextBox.Text;
+
+            try
+            {
+                connection.Open();
+                //string query = "insert into Person values('" + person.FirstName + "','" + person.LastName + "','" + person.Contact + "','" + person.LastName + "','" + pathTextBox.Text + "')";
+                string query = "update Person set FirstName='" + person.FirstName + "',LastName='" + person.LastName +
+                               "',Contact='" + person.Contact + "',Address='" + person.Address + "',Photo='" +
+                               pathTextBox.Text + "' where PersonId='" + person.PersonId + "' ";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                int affectedRow = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                firstNameTextBox.Text = "";
+                lastNameTextBox.Text = "";
+                contactTextBox.Text = "";
+                addressTextBox.Text = "";
+                pictureBox.Refresh();
+
+                MessageBox.Show("Data Update Successfully" + "\n" + affectedRow + " Row Affected");
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+            var connectionString = ConnectionString();
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            Person person = new Person();
+            person.PersonId = Convert.ToInt32(idTextBox.Text);
+
+
+            try
+            {
+                connection.Open();
+                //string query = "insert into Person values('" + person.FirstName + "','" + person.LastName + "','" + person.Contact + "','" + person.LastName + "','" + pathTextBox.Text + "')";
+                string query = "delete from Person  where PersonId='" + person.PersonId + "' ";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                int affectedRow = command.ExecuteNonQuery();
+
+                connection.Close();
+
+                firstNameTextBox.Text = "";
+                lastNameTextBox.Text = "";
+                contactTextBox.Text = "";
+                addressTextBox.Text = "";
+                pictureBox.Refresh();
+
+                MessageBox.Show("Data Update Successfully" + "\n" + affectedRow + " Row Affected");
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = ConnectionString();
+
+         
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string query = "select * from Person ";
+
+                List<Person> persons=new List<Person>();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    Person person = new Person();
+                    person.PersonId = Convert.ToInt32(reader[0]);
+                    person.FirstName = reader[1].ToString();
+                    person.LastName = reader[2].ToString();
+                    person.Contact = reader[3].ToString();
+                    person.Address = reader[4].ToString();
+                    person.Photo = (reader[5]).ToString();
+
+                    persons.Add(person);
+
+                }
+                foreach (Person person in persons)
+                {
+                    ListViewItem items=new ListViewItem();
+
+                    items.Tag = person;
+                    items.Text = person.PersonId.ToString();
+                    items.SubItems.Add(person.FirstName);
+                    items.SubItems.Add(person.LastName);
+                    items.SubItems.Add(person.Contact);
+                    items.SubItems.Add(person.Address);
+                    items.SubItems.Add(person.Photo);
+                    showListView.Items.Add(items);
+
+
+
+
+                }
+
+            }
+            catch (Exception exception)
+            {
+                {
+
+                    MessageBox.Show(exception.Message + "Not working properly");
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
